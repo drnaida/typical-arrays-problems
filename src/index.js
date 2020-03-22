7ad749c34dd410437eddb7d1a3f7f8a6ea6c0533
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-03-22 13:46:38 +0300

    feat: add max/min/average functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0 ) {
+    return 0;
+  } else {
+    let min = 0;
+    array.forEach(item => {
+      if (min > item) {
+        min = item;
+      }
+      });
+    return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0 ) {
+    return 0;
+  } else {
+    let max = array[0];
+    array.forEach(item => {
+      if (max < item) {
+        max = item;
+      }
+    });
+    return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  if (!array || array.length == 0 ) {
+    return 0;
+  } else {
+    array.forEach(item => {
+      sum += item;
+    });
+    let average = sum/array.length;
+    return average;
+  }
+
 }
